f56f2c00e8c24ef36bbefaff46e7d8dab1f6c1b8
mujahid45 <mujahid.dsi@gmail.com>
2018-11-04 16:59:52 +0000

    Latest code 3

--- cookbooks/liferay/recipes/default.rb
+++ cookbooks/liferay/recipes/default.rb
@@ -36,6 +36,15 @@ ruby_block 'Set JAVA_HOME in startup' do
     end
   end
 
+ruby_block 'Set JAVA_HOME in setenv.sh' do
+    block do
+      file = Chef::Util::FileEdit.new("#{node['tomcat']['path']}/bin/setenv.sh")
+      file.insert_line_if_no_match(/JAVA_HOME=/, "JAVA_HOME=#{node['java']['java_home']}")
+      file.insert_line_if_no_match(/JRE_HOME=/, "JRE_HOME=#{node['java']['jre_home']}")
+      file.write_file
+    end
+  end
+
 remote_file "#{tmp_path}/ehcache.tar.gz" do
   source node['ehcache']['download_url']
   mode '0777'
@@ -55,6 +64,9 @@ template "#{node['ehcache']['path']}/ehcache.xml" do
   source 'ehcache.erb'
 end
 
+template '/etc/systemd/system/liferay.service' do
+  source 'liferay.erb'
+end
 
 directory "#{node['liferay']['path']}/deploy" do
   owner 'weloadm'
@@ -66,7 +78,14 @@ cookbook_file "#{node['liferay']['path']}/deploy/licence.xml" do
   action :create
 end
 
-execute 'start the service' do
-  user 'weloadm'
-  command '/opt/SP/weloadm/software/liferay-ce-portal-7.0-ga3/tomcat-8.0.32/bin/startup.sh'
+bash "reload the system daemon" do
+  code <<-EOH
+     systemctl daemon-reload
+    EOH
 end
+
+
+service 'liferay' do
+  action [:enable, :start]
+end
+
